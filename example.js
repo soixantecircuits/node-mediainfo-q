ab043064720077c2a223bf6a89e523d30a171b11
James Morgan <james.morgan.e@googlemail.com>
2014-11-27 23:17:05 +0000

    Updating examples

--- example.js
+++ example.js
@@ -3,10 +3,9 @@
 var util = require("util"),
     mediainfo = require("./");
 
-mediainfo("test1.mp3", "test2.mp3", function(err, res) {
-  if (err) {
-    return console.log(err);
-  }
-
-  console.log(util.inspect(res, null, null, true));
-});
+mediainfo("test1.mp3", "test2.mp3")
+    .then(function (res) {
+        console.log(util.inspect(res, null, null, true));
+    }).catch(function (err) {
+        console.error(err);
+    });
